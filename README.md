72b2a88832b57f9dfd3d30355dc7cdcdd4f0bcf2
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2022-01-11 15:45:46 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Resilient Application
+The goal of this project is to practice resilience engineering in a service.
+
---
+++ src/main/java/com/example/helloresiliantapplication/HelloResiliantApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloresiliantapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloResiliantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloResiliantApplication.class, args);
+    }
+
+}
